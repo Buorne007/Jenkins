8020603b6b6020ef26ef09a2196f50e78524a7d7
Buorne007 <christonmah@gmail.com>
2021-02-19 06:44:48 -0800

    commited first java project on jenkins

---
+++ javaproject/hello.java
@@ -0,0 +1,9 @@
+public class hello{
+        public static void main(String[] args){
+                System.out.println("Hello World... Stating Countdown");
+		for(int i=1; i<=11; i++){
+                        System.out.println(11 - i);
+                }
+        }
+}
+
